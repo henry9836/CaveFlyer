e5c04b4e39bcb4fab8076d7bdec751320e43c2dd
Henry Oliver <henry983615@gmail.com>
2019-10-29 10:34:01 +1300

    Heli loads in with physics

--- CaveFlyer/GameScene.swift
+++ CaveFlyer/GameScene.swift
@@ -11,4 +11,45 @@ import GameplayKit
 
 class GameScene: SKScene {
     //Main Game
+    
+    var heli: SKSpriteNode!
+    
+    //On start
+    override func didMove(to view: SKView){
+        //self.backgroundColor = UIColor.gray
+        CreateText()
+        CreateHeli()
+    }
+    
+    //Gameloop
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    
+    func CreateText(){
+        
+    }
+    
+    func CreateHeli(){
+        heli = SKSpriteNode(texture: SKTexture(imageNamed: "f1"), size: CGSize(width: 50, height: 50))
+        heli.position = CGPoint(x: self.frame.midX-50, y: self.frame.midY)//25 - 100
+        heli.zRotation = (-25.0 * CGFloat(Double.pi/180.0))
+        
+        //Add Physics
+        heli.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        heli.physicsBody?.affectedByGravity = true
+        heli.physicsBody?.isDynamic = true
+        heli.physicsBody?.allowsRotation = false
+        heli.physicsBody?.mass = 1.0
+        
+        
+        
+        let heliAtlas = SKTextureAtlas(named: "Helicopter")
+        let flyAction = SKAction.animate(with: [heliAtlas.textureNamed("f1"), heliAtlas.textureNamed("f2")], timePerFrame: 0.1)
+        
+        heli.run(SKAction.repeatForever(flyAction))
+        
+        self.addChild(heli)
+        
+    }
 }
