35e2cba8946bf49209b8b3d92dc193baf8541983
Henry Oliver <henry983615@gmail.com>
2019-10-29 11:22:14 +1300

    Heli has speed cap

--- CaveFlyer/GameScene.swift
+++ CaveFlyer/GameScene.swift
@@ -13,17 +13,68 @@ class GameScene: SKScene {
     //Main Game
     
     var heli: SKSpriteNode!
-    
+    var holdGestureRecognizer = UILongPressGestureRecognizer()
+    var holding: Bool?
+    var touchFlySide: Bool? //true = flyUp, //false = shoot
     //On start
     override func didMove(to view: SKView){
         //self.backgroundColor = UIColor.gray
+        holding = false
+        touchFlySide = false
+        RemoveGestures()
         CreateText()
         CreateHeli()
     }
     
+    //Input
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        holding = true
+        if let location =  touches.first?.location(in: self.view){
+            if (location.x < self.frame.midX){ //user tapped on left side of screen
+                touchFlySide = true
+            }
+            else{ //user tapped on right side of screen
+                touchFlySide = false
+            }
+            
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        holding = false
+    }
+    
     //Gameloop
     override func update(_ currentTime: TimeInterval) {
-        
+        //
+        if (holding == true){
+            if (touchFlySide == true){
+                //cap fly up speed
+                let yVelo: Float
+                yVelo = Float(heli.physicsBody?.velocity.dy ?? CGFloat(0))
+                
+                if (yVelo < 400){
+                    //fly up
+                    heli.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy:50.0))
+                    
+                    print(yVelo)
+                }
+            }
+            else{
+                //pew pew
+                heli.position = CGPoint(x: self.frame.midX-50, y: self.frame.midY)//25 - 100
+                heli.physicsBody?.affectedByGravity = false
+                heli.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
+            }
+        }
+        else{
+            heli.physicsBody?.affectedByGravity = true
+        }
+    }
+    
+    func RemoveGestures(){
+        view?.gestureRecognizers?.removeAll()
     }
     
     func CreateText(){
--- CaveFlyer/GameViewController.swift
+++ CaveFlyer/GameViewController.swift
@@ -31,19 +31,5 @@ class GameViewController: UIViewController {
         }
     }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+   
 }
